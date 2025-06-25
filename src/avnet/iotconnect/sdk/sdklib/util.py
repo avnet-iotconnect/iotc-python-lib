8cc3770a4226cc6c9e50c82a4e359c0ab020a5c4
zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>
2025-06-24 18:29:39 -0600

    Added millisecond-precision to timestamp function
    Signed-off-by: zackandraka-avnet <110255705+zackandraka-avnet@users.noreply.github.com>

--- src/avnet/iotconnect/sdk/sdklib/util.py
+++ src/avnet/iotconnect/sdk/sdklib/util.py
@@ -12,7 +12,8 @@ from typing import get_type_hints, Type, Union, TypeVar
 
 
 def to_iotconnect_time_str(ts: datetime) -> str:
-    return ts.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+    ns_str = str(int(ts.microsecond / 1000))
+    return ts.strftime(f"%Y-%m-%dT%H:%M:%S.{ns_str}Z")
 
 
 def dict_filter_empty(input_dict: dict):
